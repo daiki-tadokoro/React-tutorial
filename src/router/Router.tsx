dadea101e63099b9c8230e90976304647ffe40ff
田所大樹 <tadokoro@crewinc.io>
2022-01-06 06:42:09 +0900

    add 404page

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,14 +2,32 @@ import { memo, VFC } from "react";
 import { Route, Switch } from "react-router-dom";
 
 import { Login } from "../components/pages/Login"
+import { Page404 } from "../components/pages/Page404";
+import { HomeRouters } from "./HomeRouters";
 
 
 export const Router: VFC = memo(() => {
   return (
-      <Switch>
-          <Route exact path="/">
-              <Login />
-          </Route>
-      </Switch>
+    <Switch>
+      <Route exact path="/">
+        <Login />
+      </Route>
+      <Route path="/home" render={({ match: {url} }) => (
+        <Switch>
+          { HomeRouters.map((route) => (
+              <Route 
+                key={route.path}
+                exact={route.exact}
+                path={`${url}${route.path}`}
+              >
+                  {route.children}
+              </Route>
+          ))}
+        </Switch>
+      )} />
+      <Route path="*">
+          <Page404 />
+      </Route>
+    </Switch>
   )
 })
